db322232a5700acf6456ab7d7a69e525def99db6
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-19 11:52:18 +0600

    complete my profile design

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
        {/*-----------@copyright--------------*/}
        <div>
           <hr />
-          <p className='py-5 text-sm text-center'>Copyright © 2024 GreatStack - All Right Reserved</p>
+          <p className='py-5 text-sm text-center'>Copyright © 2024 - All Right Reserved</p>
        </div>
     </div>
   )
