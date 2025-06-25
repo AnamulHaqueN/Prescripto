fd2290f08f24271051c10d4ac80bb5a7c5bfd9c3
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-25 09:38:36 +0600

    add doctor login API

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -4,14 +4,16 @@ import jwt from 'jsonwebtoken'
 const authAdmin = async (req, res, next) => {
     try {
 
-        const {atoken} = req.headers
+        const atoken = req.headers.atoken || req.headers['atoken'];
+
         if(!atoken) {
             return res.json({success:false, message:'Not Authorized Login Again'})
         }
-        const token_decode = jwt.verify(atoken, process.env.JWT_SECRET)
-        if(token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-            return res.json({success:false, message:'Not Authorized Login Again'})
+        const decoded = jwt.verify(atoken, process.env.JWT_SECRET);
+            if (!decoded || decoded.role !== "admin") {
+            return res.json({ success: false, message: 'Not Authorized Login Again' });
         }
+
         next()
 
     } catch(error) {
