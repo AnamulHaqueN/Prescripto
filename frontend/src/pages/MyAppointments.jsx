3423c9ddaec8d2bec4c3c6384892390e85745210
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-27 17:26:13 +0600

    Updated MyAppointments page for doctor appointment functionality

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -9,20 +9,7 @@ const MyAppointments = () => {
 
   const [appointments, setAppointments] = useState([]);
 
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
+  const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",];
 
   const slotDateFormat = (slotDate) => {
     const dateArray = slotDate.split("_");
@@ -95,6 +82,8 @@ const MyAppointments = () => {
     }
   }, [token]);
 
+
+
   return (
     <div>
       <p className="pb-3 mt-12 font-medium text-zinc-700 border-b ">
@@ -134,7 +123,7 @@ const MyAppointments = () => {
               {!item.cancelled && !item.payment && (<button onClick={() => handlePayment(item._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 hover:bg-primary hover:text-white transition-all duration-300">Pay Online</button>)}
               {item.paid && !item.cancelled && (<button className="sm:min-w-48 py-2 border border-green-500 rounded text-green-500">Payment Completed</button>)}
               {!item.cancelled && (<button onClick={() => cancelAppointment(item._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 hover:bg-red-600 hover:text-white transition-all duration-300 ">  Cancel appointment</button>)}
-              {item.cancelled && (<button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500">Appointment cancelled</button>)}
+              {item.cancelled && (<button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500">Appointment cancelled</button>)} 
             </div>
           </div>
         ))}
