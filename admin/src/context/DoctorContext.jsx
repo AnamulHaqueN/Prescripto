ed2e6e10903b083f048aeb4b072566abac374e8e
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-27 17:22:53 +0600

    added appointments API for doctor pannel

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,14 +1,80 @@
+import axios from 'axios'
 import {createContext, useState} from 'react'
 export const DoctorContext = createContext()
+import { toast } from 'react-toastify';
 
 const DoctorContextProvider = (props) => {
   
   const backendUrl = import.meta.env.VITE_BACKEND_URL
   const [dToken, setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
+  const [appointments, setAppointments] = useState([])
+
+  const getAppointment = async () => {
+      
+    try {
+
+      const {data} = await axios.get(backendUrl + '/api/doctor/appointments', {headers:{dToken}})
+
+      if(data.success) {
+        setAppointments(data.appointments)
+        console.log(data.appointments)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+
+  }
+
+  const completeAppointment = async (appointmentId) => {
+    
+    try {
+
+      const {data} = await axios.post(backendUrl + '/api/doctor/complete-appointment', {appointmentId}, {headers: {dtoken: dToken}})
+
+      if(data.success) {
+        toast.success(data.message)
+        getAppointment()
+      } else {
+        toast.error(data.error)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+
+  }
+
+  const cancelAppointment = async(appointmentId) => {
+
+    try {
+
+      const {data} = await axios.post(backendUrl + '/api/doctor/cancel-appointment', {appointmentId}, {headers: {dtoken: dToken}})
+
+      if(data.success) {
+        toast.success(data.message)
+        getAppointment()
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+
+  }
 
   const value = {
     dToken, setDToken,
-    backendUrl
+    backendUrl, 
+    appointments, setAppointments,
+    getAppointment,
+    completeAppointment, cancelAppointment,
   }
 
   return (
